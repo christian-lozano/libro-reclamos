4fe22e6eb70177dccc25ca9de29f49d0cfd9102b
christian lozano <christian-lozano>
2023-10-23 10:33:01 -0500

    add libro de reclamaciones col-1

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -219,7 +219,7 @@ export const Footer = memo(function Footer() {
               <h6 className="mb-3 text-black text-2xl font-semibold text-center">
                 Libro de Reclamaciones
               </h6>
-              <div className="grid grid-cols-2 laptop:grid-cols-1 2xl:grid-cols-2 2xl:gap-y-7 gap-x-1 gap-y-2  ">
+              <div className="grid grid-cols-1 sm:grid-cols-1 laptop:grid-cols-1 2xl:grid-cols-2 2xl:gap-y-7 gap-x-1 gap-y-2  ">
                 <Input
                   size="lg"
                   label="Dirección de Domicilio"
