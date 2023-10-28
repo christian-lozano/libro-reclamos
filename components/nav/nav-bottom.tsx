3db1bf4dcacdc245aca498748fdaac4465c44abb
christian lozano <christian-lozano>
2023-10-28 13:43:34 -0500

    pl menu mobil left-2

--- components/nav/nav-bottom.tsx
+++ components/nav/nav-bottom.tsx
@@ -46,7 +46,7 @@ export function NavBottom() {
     <div className="flex items-center mr-1 relative divide-x border-b border-neutral-light laptop:h-12 laptop:mx-20 laptop:mb-5 laptop:px-0 laptop:justify-between laptop:border-none laptop:divide-none">
       <Tablet>
         <Button
-          className="p-3 pl-0 2xl:hidden xl:hidden"
+          className="p-3 pl-2 2xl:hidden xl:hidden"
           onClick={() => handleSidebarToggle(true)}
         >
           <IconLabel icon={MenuIcon} label="Menu" labelPosition="right" />
