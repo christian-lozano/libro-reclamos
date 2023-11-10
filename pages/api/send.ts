0057776ef04ac27cae4c6d0b4b6c7985f0ad17ea
christian lozano <christian-lozano>
2023-11-10 11:33:40 -0500

    add form send

---
+++ pages/api/send.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Resend } from 'resend'
+
+import { emailTemplate } from '@/components/email-template-send/EmailTemplate'
+
+const resend = new Resend('re_CApSewKk_Lb6awHRVv2deVEfmsfDbEUWm')
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['christian.lozano.adrianzen@gmail.com'],
+      subject: 'Hello world',
+      react: emailTemplate({ firstName: 'Renny' }),
+    })
+
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
