5bf4ca86e76e4acf80681047e53cb8ad12664734
christian lozano <christian-lozano>
2023-11-11 12:38:55 -0500

    add correo libro de reclamaciones

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend'
 
 import { emailTemplate } from '@/components/email-template-send/EmailTemplate'
 
-const resend = new Resend('re_CApSewKk_Lb6awHRVv2deVEfmsfDbEUWm')
+const resend = new Resend('re_RoBWXNh3_GsmCRJFrV8MtJ5bdUnqsu96H')
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -19,7 +19,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     } = req.body
     const data = await resend.emails.send({
       from: 'Fritz Sport <onboarding@resend.dev>',
-      to: ['christian.lozano.adrianzen@gmail.com'],
+      to: ['servicioalclientefritzsport@gmail.com'],
       subject: 'Libro de Reclamaciones',
       react: emailTemplate({
         nombre,
