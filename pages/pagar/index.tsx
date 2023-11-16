5ccb35139e7fe399cca6d61752833e3cc5077a99
christian lozano <christian-lozano>
2023-11-16 11:46:15 -0500

    add tyc , pyp3

--- pages/pagar/index.tsx
+++ pages/pagar/index.tsx
@@ -1,4 +1,3 @@
-import { Checkbox } from '@material-tailwind/react'
 import React, { useEffect, useState } from 'react'
 import { useCart } from 'react-use-cart'
 
@@ -571,13 +570,37 @@ export default function Home(props: SearchPageLayoutProps) {
               </div>
 
               <div className=" xl:flex items-center hidden  z-10 justify-center">
-                <Checkbox
-                  className="z-10"
-                  nonce={undefined}
-                  onResize={undefined}
-                  onResizeCapture={undefined}
-                  onClick={(e) => setCheckoutPago(e.target.checked)}
-                />
+                <div className="inline-flex items-center">
+                  <label
+                    className="relative flex cursor-pointer items-center rounded-full p-3"
+                    htmlFor="checkbox-8"
+                    data-ripple-dark="true"
+                  >
+                    <input
+                      // checked={checkoutPago}
+                      type="checkbox"
+                      className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-black checked:bg-black checked:before:bg-black hover:before:opacity-10"
+                      id="checkbox-8"
+                      onChange={(e) => setCheckoutPago(e.target.checked)}
+                    />
+                    <div className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-3.5 w-3.5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        stroke="currentColor"
+                        strokeWidth="1"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                  </label>
+                </div>
                 <span className="text-gray-400 text-sm  "> Acepto los</span>{' '}
                 <Link
                   href="/tyc"
