c14759a7966067db294ba62a48a11a3cd45d65a8
christian lozano <christian-lozano>
2023-11-20 13:32:31 -0500

    add test form multiple products2

--- pages/mujer/index.tsx
+++ pages/mujer/index.tsx
@@ -1,4 +1,3 @@
-import { doc, setDoc } from 'firebase/firestore'
 import Link from 'next/link'
 import { useCallback, useEffect, useState } from 'react'
 import { Configure } from 'react-instantsearch-dom'
@@ -11,214 +10,213 @@ import {
   getStaticPropsPage,
   SearchPageLayout,
 } from '@/layouts/search-page-layout'
-import { db } from '@/utils/firebase'
 
-const test = async () => {
-  // Add a new document in collection "cities"
-  await setDoc(doc(db, 'products', 'LA'), {
-    sku: 'IF7417',
-    parentID: 'IF7417',
-    name: 'ZAPATILLAS TERREX VOYAGER 21 TRAVEL',
-    description: '',
-    slug: 'pantalon-adibreak-adicolor-classics',
-    brand: 'Adidas',
-    gender: 'Hombre',
-    hierarchical_categories: {
-      lvl0: 'Hombre',
-      lvl1: 'Hombre > Calzado',
-      lvl2: 'Hombre > Calzado > Zapatillas',
-    },
-    list_categories: ['Hombre', 'Calzado', 'Zapatillas', 'Basquet', 'Terrex'],
-    category_page_id: ['Hombre  > Calzado', 'Hombre  > Calzado > Zapatillas'],
-    image_urls: [
-      'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/73fba391168c45059ecacaaf98189aa4_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_01_standard.jpg',
-      'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2f6a24ddd8204ab3aa9decfbbb4ed42e_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_02_standard_hover.jpg',
-      'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/48e8043f97a943b19494d06d25b70f11_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_03_standard.jpg',
-      'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ca5940ed73204ba28a4551468d50bdb4_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_04_standard.jpg',
-    ],
-    image_blurred: 'LNSPX]M{-;xu_4xuIURjRiofoMWA',
-    reviews: {
-      rating: 4,
-      count: 99,
-      bayesian_avg: 3.96,
-    },
-    color: {
-      filter_group: 'negro;#000',
-      original_name: 'negro',
-    },
-    available_sizes: [
-      '34',
-      '34.5',
-      '35',
-      '35.5',
-      '36',
-      '36.5',
-      '37',
-      '37.5',
-      '38',
-      '38.5',
-      '39',
-      '39.5',
-      '40',
-      '40.5',
-      '41',
-      '41.5',
-      '42',
-      '42.5',
-      '43',
-      '43.5',
-      '44',
-      '44.5',
-      '45',
-      '45.5',
-      '46',
-    ],
-    variants: [
-      {
-        sku: 'M0E20000000EAAL',
-        abbreviated_size: '34.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAM',
-        abbreviated_size: '35',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAN',
-        abbreviated_size: '35.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAO',
-        abbreviated_size: '36',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAP',
-        abbreviated_size: '36.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAQ',
-        abbreviated_size: '37',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAR',
-        abbreviated_size: '37.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAS',
-        abbreviated_size: '38',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAT',
-        abbreviated_size: '38.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAU',
-        abbreviated_size: '39',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAV',
-        abbreviated_size: '39.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAW',
-        abbreviated_size: '40',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAX',
-        abbreviated_size: '40.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAY',
-        abbreviated_size: '41',
-        in_stock: true,
-      },
-      {
-        sku: 'M0E20000000EAAZ',
-        abbreviated_size: '41.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB0',
-        abbreviated_size: '42',
-        in_stock: true,
-      },
-      {
-        sku: 'M0E20000000EAB1',
-        abbreviated_size: '42.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB2',
-        abbreviated_size: '43',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB3',
-        abbreviated_size: '43.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB4',
-        abbreviated_size: '44',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB5',
-        abbreviated_size: '44.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB6',
-        abbreviated_size: '45',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB7',
-        abbreviated_size: '45.5',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAB8',
-        abbreviated_size: '46',
-        in_stock: false,
-      },
-      {
-        sku: 'M0E20000000EAAK',
-        abbreviated_size: '34',
-        in_stock: false,
-      },
-    ],
-    price: {
-      currency: 'EUR',
-      value: 253,
-      discounted_value: 0,
-      discount_level: -100,
-      on_sales: false,
-    },
-    units_in_stock: 0,
-    created_at: 0,
-    updated_at: 0,
-    related_products: null,
-    product_type: 'Zapatillas',
-    objectID: 'IF7417',
-  })
-  const cityRef = doc(db, 'products', 'BJ')
-  setDoc(cityRef, { capital: true }, { merge: true })
-}
+// const test = async () => {
+//   // Add a new document in collection "cities"
+//   await setDoc(doc(db, 'products', 'LA'), {
+//     sku: 'IF7417',
+//     parentID: 'IF7417',
+//     name: 'ZAPATILLAS TERREX VOYAGER 21 TRAVEL',
+//     description: '',
+//     slug: 'pantalon-adibreak-adicolor-classics',
+//     brand: 'Adidas',
+//     gender: 'Hombre',
+//     hierarchical_categories: {
+//       lvl0: 'Hombre',
+//       lvl1: 'Hombre > Calzado',
+//       lvl2: 'Hombre > Calzado > Zapatillas',
+//     },
+//     list_categories: ['Hombre', 'Calzado', 'Zapatillas', 'Basquet', 'Terrex'],
+//     category_page_id: ['Hombre  > Calzado', 'Hombre  > Calzado > Zapatillas'],
+//     image_urls: [
+//       'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/73fba391168c45059ecacaaf98189aa4_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_01_standard.jpg',
+//       'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2f6a24ddd8204ab3aa9decfbbb4ed42e_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_02_standard_hover.jpg',
+//       'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/48e8043f97a943b19494d06d25b70f11_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_03_standard.jpg',
+//       'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ca5940ed73204ba28a4551468d50bdb4_9366/Zapatillas_Terrex_Voyager_21_Travel_Plomo_IF7417_04_standard.jpg',
+//     ],
+//     image_blurred: 'LNSPX]M{-;xu_4xuIURjRiofoMWA',
+//     reviews: {
+//       rating: 4,
+//       count: 99,
+//       bayesian_avg: 3.96,
+//     },
+//     color: {
+//       filter_group: 'negro;#000',
+//       original_name: 'negro',
+//     },
+//     available_sizes: [
+//       '34',
+//       '34.5',
+//       '35',
+//       '35.5',
+//       '36',
+//       '36.5',
+//       '37',
+//       '37.5',
+//       '38',
+//       '38.5',
+//       '39',
+//       '39.5',
+//       '40',
+//       '40.5',
+//       '41',
+//       '41.5',
+//       '42',
+//       '42.5',
+//       '43',
+//       '43.5',
+//       '44',
+//       '44.5',
+//       '45',
+//       '45.5',
+//       '46',
+//     ],
+//     variants: [
+//       {
+//         sku: 'M0E20000000EAAL',
+//         abbreviated_size: '34.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAM',
+//         abbreviated_size: '35',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAN',
+//         abbreviated_size: '35.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAO',
+//         abbreviated_size: '36',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAP',
+//         abbreviated_size: '36.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAQ',
+//         abbreviated_size: '37',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAR',
+//         abbreviated_size: '37.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAS',
+//         abbreviated_size: '38',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAT',
+//         abbreviated_size: '38.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAU',
+//         abbreviated_size: '39',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAV',
+//         abbreviated_size: '39.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAW',
+//         abbreviated_size: '40',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAX',
+//         abbreviated_size: '40.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAY',
+//         abbreviated_size: '41',
+//         in_stock: true,
+//       },
+//       {
+//         sku: 'M0E20000000EAAZ',
+//         abbreviated_size: '41.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB0',
+//         abbreviated_size: '42',
+//         in_stock: true,
+//       },
+//       {
+//         sku: 'M0E20000000EAB1',
+//         abbreviated_size: '42.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB2',
+//         abbreviated_size: '43',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB3',
+//         abbreviated_size: '43.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB4',
+//         abbreviated_size: '44',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB5',
+//         abbreviated_size: '44.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB6',
+//         abbreviated_size: '45',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB7',
+//         abbreviated_size: '45.5',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAB8',
+//         abbreviated_size: '46',
+//         in_stock: false,
+//       },
+//       {
+//         sku: 'M0E20000000EAAK',
+//         abbreviated_size: '34',
+//         in_stock: false,
+//       },
+//     ],
+//     price: {
+//       currency: 'EUR',
+//       value: 253,
+//       discounted_value: 0,
+//       discount_level: -100,
+//       on_sales: false,
+//     },
+//     units_in_stock: 0,
+//     created_at: 0,
+//     updated_at: 0,
+//     related_products: null,
+//     product_type: 'Zapatillas',
+//     objectID: 'IF7417',
+//   })
+//   const cityRef = doc(db, 'products', 'BJ')
+//   setDoc(cityRef, { capital: true }, { merge: true })
+// }
 
-test()
+// test()
 const data = {
   // --------------------------------------
   dataPageMujer: {
