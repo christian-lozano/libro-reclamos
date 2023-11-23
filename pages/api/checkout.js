1dbf6d7cc50f6995ba8489fb225f9b13924a124f
christian lozano <christian-lozano>
2023-11-23 13:01:25 -0500

    add  stcok not add product cart10

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -84,7 +84,7 @@ console.log(req.body);
       },
     },
     back_urls: {
-      success: "/api/exito",
+      success: `http://fritz-sport.vercel.app/api/exito?variable1=${req.body.stock}`,
       failure: "fritz-sport.vercel.app",
       pending: "fritz-sport.vercel.app",
     },
