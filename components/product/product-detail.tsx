13042c5caa4360782cb4d38f4ca8449a6511dadc
christian lozano <christian-lozano>
2023-11-27 18:47:24 -0500

    add remove pantalla celeste2

--- components/product/product-detail.tsx
+++ components/product/product-detail.tsx
@@ -25,40 +25,40 @@ export function ProductDetails({
   // )
 
   return (
-    <div
-      className={classNames(
-        'w-full cursor-pointer flex  justify-center mb-3',
-        className
-      )}
-    >
-      <div className="flex flex-col items-center  w-full h-2/4">
-        <div className="hidden xl:block">
-          <ProductZoomImgDetail mainimg={mainImg} />
-        </div>
-        <img
-          className={classNames(
-            'rounded-md w-full hover:translate-y-1 delay-100 ease-in-out xl:hidden block'
-          )}
-          src={mainImg}
-          // alt={alt}
-        />
-        <div className="flex justify-start items-start  dark:bg-[var(--dark-mode)] py-1 bg-white">
-          <div className="grid grid-flow-col gap-x-2 p-1">
-            {src.map((el, i) => (
-              // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-              <div key={i} onClick={() => setMainImg(src[i])}>
-                <img
-                  className={classNames(
-                    'rounded-md w-[4rem] hover:translate-y-1 delay-100 ease-in-out'
-                  )}
-                  src={el}
-                  alt={alt}
-                />
-              </div>
-            ))}
+    <>
+      <img
+        className="rounded-md w-full hover:translate-y-1 delay-100 ease-in-out xl:hidden block"
+        src={mainImg}
+        // alt={alt}
+      />
+      <div
+        className={classNames(
+          'w-full cursor-pointer flex  justify-center mb-3',
+          className
+        )}
+      >
+        <div className="flex flex-col items-center  w-full h-2/4">
+          <div className="hidden xl:block">
+            <ProductZoomImgDetail mainimg={mainImg} />
           </div>
-        </div>{' '}
+          <div className="flex justify-start items-start  dark:bg-[var(--dark-mode)] py-1 bg-white">
+            <div className="grid grid-flow-col gap-x-2 p-1">
+              {src.map((el, i) => (
+                // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+                <div key={i} onClick={() => setMainImg(src[i])}>
+                  <img
+                    className={classNames(
+                      'rounded-md w-[4rem] hover:translate-y-1 delay-100 ease-in-out'
+                    )}
+                    src={el}
+                    alt={alt}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>{' '}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
