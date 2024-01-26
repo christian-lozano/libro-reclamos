14e731a9dda6122384bb0dc502c10be452ab6672
christian lozano <christian-lozano>
2024-01-26 17:11:28 -0500

    restableci env

--- components/panel/FilterSearch.tsx
+++ components/panel/FilterSearch.tsx
@@ -12,10 +12,7 @@ import type { InstantSearchServerState } from 'react-instantsearch-hooks'
 
 import { SearchBox } from '@/components/panel/SearchBox'
 
-const client = algoliasearch(
-  `${process.env.CLI_APP_ID}`,
-  `${process.env.CLI_ADMIN_API_KEY}`
-)
+const client = algoliasearch('235XIUIEK1', '32f92a7d31a7320106285b5b7466e336')
 
 export default function FilterSeach({
   serverState,
@@ -33,7 +30,7 @@ export default function FilterSeach({
     <InstantSearchSSRProvider {...serverState}>
       <InstantSearch
         searchClient={client}
-        indexName={`${process.env.NEXT_PUBLIC_INSTANTSEARCH_INDEX_NAME}`}
+        indexName="pwa_ecom_ui_template_products"
         routing={{
           router: history({
             getLocation() {
--- pages/api/exito2.js
+++ pages/api/exito2.js
@@ -35,11 +35,8 @@ export default async function handler(req, res) {
                         return productos
                       })
                       console.log(productosCantidad);
-           const client = algoliasearch(
-                              `${process.env.CLI_APP_ID}`,
-                              `${process.env.CLI_ADMIN_API_KEY}`
-                            )
-                          const index = client.initIndex(`${process.env.NEXT_PUBLIC_INSTANTSEARCH_INDEX_NAME}`)
+                        const client = algoliasearch('235XIUIEK1','32f92a7d31a7320106285b5b7466e336')
+                        const index = client.initIndex('pwa_ecom_ui_template_products')
                           index.partialUpdateObjects(productosCantidad)
                           .then(({ objectIDs }) => {
                             console.log(objectIDs);
