dc1d43e3a6fca56c695825d8ce43ef209c1554d4
christian lozano <christian-lozano>
2024-02-26 13:19:03 -0500

    guia de tallas mobil

--- components/guia-tallas/GiaDeTallasRopa.js
+++ components/guia-tallas/GiaDeTallasRopa.js
@@ -5,8 +5,7 @@ import { Card, Typography } from "@material-tailwind/react";
  
 export default function GiaDeTallasRopa({GiaDeTallasRopa}) {
   return (
-    <Card className="h-[100vh] w-full overflow-y-scroll p-8" >
-
+    <Card className="laptop:h-[100vh] xl:h-[100vh] w-full overflow-y-scroll 2xl:p-8 py-3 px-3" >
       <div className="text-3xl text-black font-extrabold text-center uppercase mb-5">{GiaDeTallasRopa.title}</div>
       <table className="w-full min-w-max table-auto text-left h-full">
         <thead>
@@ -16,7 +15,8 @@ export default function GiaDeTallasRopa({GiaDeTallasRopa}) {
                 <Typography
          
             
-                  className="font-extrabold text-white uppercase  leading-none opacity-70 text-center"
+                  className="font-extrabold text-white uppercase 2xl:text-base text-sm leading-none opacity-70 text-center"
+
                 >
                   {head}
                 </Typography>
@@ -28,33 +28,33 @@ export default function GiaDeTallasRopa({GiaDeTallasRopa}) {
           {GiaDeTallasRopa.TABLE_ROWS.map(({TITLE,ADIDAS, REEBOK,NIKE, CAT,FILA}, index) => (
             <tr key={index} className="even:bg-blue-gray-50/50">
             <td className="">
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+              <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                   {TITLE}
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+               <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                   {ADIDAS}
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+              <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {REEBOK}
 
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+             <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {NIKE}
 
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+              <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {CAT}
 
                 </Typography>
@@ -63,7 +63,7 @@ export default function GiaDeTallasRopa({GiaDeTallasRopa}) {
                 FILA && (
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+             <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {FILA}
 
                 </Typography>
--- components/guia-tallas/GiaDeTallasZapatillas.js
+++ components/guia-tallas/GiaDeTallasZapatillas.js
@@ -5,18 +5,18 @@ import { Card, Typography } from "@material-tailwind/react";
  
 export default function GiaDeTallasZapatillas({dataTallasZapatillas}) {
   return (
-    <Card className="h-[100vh] w-full overflow-y-scroll p-8" >
+    <Card className="laptop:h-[100vh] xl:h-[100vh] w-full overflow-y-scroll 2xl:p-8 py-3 px-3" >
 
-      <div className="text-3xl text-black font-extrabold text-center uppercase mb-5">{dataTallasZapatillas.title}</div>
+      <div className="xl:text-3xl text-black font-extrabold text-center uppercase mb-5">{dataTallasZapatillas.title}</div>
       <table className="w-full min-w-max table-auto text-left h-full">
         <thead>
           <tr className="sticky top-0 mb-10">
             {dataTallasZapatillas.TABLE_HEAD.map((head) => (
-              <th key={head} className="border-b border-blue-gray-100 bg-black p-4">
+              <th key={head} className="border-b border-blue-gray-100 bg-black 2xl:p-4 p-1">
                 <Typography
          
             
-                  className="font-extrabold text-white uppercase  leading-none opacity-70 text-center"
+                  className="font-extrabold text-white uppercase 2xl:text-base text-sm leading-none opacity-70 text-center"
                 >
                   {head}
                 </Typography>
@@ -28,33 +28,33 @@ export default function GiaDeTallasZapatillas({dataTallasZapatillas}) {
           {dataTallasZapatillas.TABLE_ROWS.map(({TITLE,ADIDAS, REEBOK,NIKE, CAT,FILA}, index) => (
             <tr key={index} className="even:bg-blue-gray-50/50">
             <td className="">
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                   {TITLE}
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                   {ADIDAS}
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {REEBOK}
 
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {NIKE}
 
                 </Typography>
               </td>
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {CAT}
 
                 </Typography>
@@ -63,7 +63,7 @@ export default function GiaDeTallasZapatillas({dataTallasZapatillas}) {
                 FILA && (
               <td className="">
 
-                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 px-5 py-5 h-full text-center">
+                <Typography variant="small" color="blue-gray" className="font-normal hover:bg-blue-gray-100 2xl:px-5 2xl:py-5 flex justify-center 2xl:text-base text-sm h-full text-center">
                 {FILA}
 
                 </Typography>
