4213317bcc0d5ca6390d2a5228addd37a3016efa
christian lozano <christian-lozano>
2024-02-28 18:53:56 -0500

    add skeleto product7

--- components/product-card/product-card-skeleton.js
+++ components/product-card/product-card-skeleton.js
@@ -20,7 +20,7 @@ export default function  ProductCardSkeleton() {
         </svg>
     </div>
     <div class="w-full">
-        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 mb-4"></div>
         <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
         <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
 
