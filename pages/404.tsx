adefa83a5f75a485116510ad88881176f36c46c9
christian lozano <christian-lozano>
2024-03-13 18:06:35 -0500

    add pagina 404 noy found

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import NotFound from '@/components/404/notFound'
 import { HeaderNew } from '@/components/header/header-new'
 import type { SearchPageLayoutProps } from '@/layouts/search-page-layout'
@@ -31,37 +30,3 @@ export const getStaticProps = async () => {
     revalidate: 80, // In seconds
   }
 }
-=======
-import NotFound from '@/components/404/notFound'
-import { HeaderNew } from '@/components/header/header-new'
-import type { SearchPageLayoutProps } from '@/layouts/search-page-layout'
-import {
-  SearchPageLayout,
-  getStaticPropsPage,
-} from '@/layouts/search-page-layout'
-
-export default function Home(props: SearchPageLayoutProps) {
-  return (
-    <SearchPageLayout {...props}>
-      <HeaderNew activeSearchGlobal={false} props={props} />
-      <NotFound />
-    </SearchPageLayout>
-  )
-}
-export const getStaticProps = async () => {
-  const resNav = await fetch('https://www.fritzsport.pe/api/home/nav')
-  const resLogo = await fetch('https://www.fritzsport.pe/api/home/logo')
-  const homeNav = await resNav.json()
-  const homeLogo = await resLogo.json()
-
-  getStaticPropsPage(Home)
-  return {
-    props: {
-      homeNav,
-      homeLogo,
-    },
-    // MujerPortadaDesktop,
-    revalidate: 80, // In seconds
-  }
-}
->>>>>>> f065f0f790ecdb1d3b9e59ce6d324d46cb66c0e3
